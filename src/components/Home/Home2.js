c57b97c54e9809ebe99f56bb9e78b1f5b58d4211
devOps-Code-Commit <singhsantosh.edu.in@gmail.com>
2023-12-13 10:08:04 +0530

    setup gh-pages

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -64,78 +64,7 @@ function Home2() {
             </Tilt>
           </Col>
         </Row>
-        {/* <Row>
-          <Col md={8} className="home-about-description2">
-            <h1 style={{ fontSize: "2.6em" }}>
-              <span className="purple"> Skills </span>
-            </h1>
-            <p className="home-about-body">
-              Node.js | Nest.js:
-              <br />
-              Expertise in developing efficient server-side applications.
-            </p>
 
-            <p className="home-about-body">
-              AWS | AwsLambda | SQS | SNS:
-              <br />
-              Proficient in leveraging Amazon Web Services for scalable solutions.
-            </p>
-
-            <p className="home-about-body">
-              Express.js:
-              <br />
-              Proficient in building RESTful APIs for seamless communication.
-            </p>
-
-            <p className="home-about-body">
-              Databases:
-              <br />
-              Management of MongoDB, PostgreSQL, and MySQL databases.
-            </p>
-
-            <p className="home-about-body">
-              Typescript:
-              <br />
-              Utilizing TypeScript to write robust and maintainable code.
-            </p>
-
-            <p className="home-about-body">
-              Git Platforms:
-              <br />
-              Proficient in version control systems like GitHub, Bit Bucket, GitLab.
-            </p>
-
-            <p className="home-about-body">
-              Containerization:
-              <br />
-              Experience with Docker for efficient application containerization.
-            </p>
-
-            <p className="home-about-body">
-              Project Management:
-              <br />
-              Efficient use of tools like Trello and Jira for project organization.
-            </p>
-
-            <p className="home-about-body">
-              Development Environments:
-              <br />
-              Familiarity with VS Code and other development tools.
-            </p>
-
-            <p className="home-about-body">
-              AWS Services:
-              <br />
-              Handling AWS resources, such as EC2 instances and S3 buckets.
-            </p>
-
-            <p className="home-about-body">
-              Operating Systems:
-              <br />
-              Proficient in Mac and Linux environments.
-            </p>
-          </Col>
-        </Row> */}
         <Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
